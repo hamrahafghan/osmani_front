ae0673553605ebe51bffc2eae518fc6ee2b6d556
hamrahafghan <142744735+hamrahafghan@users.noreply.github.com>
2026-05-17 08:55:25 +0430

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 // lib/api.ts
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://osmani-backend.onrender.com/api';
 
 // ============================================
 // موجودیت‌ها (Interfaces)
